9b24d650776a291437425755887382785f100b92
Handokodwi38 <52731750+Handokodwi38@users.noreply.github.com>
2019-07-29 21:55:27 +0700

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,30 @@
+package tugasstrukturdata;
+
+import java.util.Arrays;
+
+public class Array {
+    public static void main(String[] args) {
+        int[] x = {2, 4, 6};
+        int[] z = {8, 10, 16};
+        int sumX = 0;
+        int sumZ = 0;
+        int jumlah;
+        
+        for(int num : x){
+            sumX = sumX+num;
+        }
+        for(int num : z){
+            sumZ = sumZ+num;
+        }
+        jumlah = sumX + sumZ;
+        
+        System.out.println("Jumlah Nilai Kedua Array : "+jumlah);
+        
+        int[] xz = new int[x.length+z.length];
+        System.arraycopy(x, 0, xz, 0, z.length);
+        System.arraycopy(z, 0, xz, x.length, z.length);
+        
+        System.out.println("Menggabungkan Kedua Array");
+        System.out.println(Arrays.toString(xz));
+    }
+}
